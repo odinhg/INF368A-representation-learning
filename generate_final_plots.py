b8fda603fb69b79a069d27894bcf9aded61e02d3
odgar2833 <odgar2833@birget.uib.no>
2022-12-07 15:25:48 +0100

    updated plots

--- generate_final_plots.py
+++ generate_final_plots.py
@@ -12,12 +12,12 @@ for model in models.keys():
     train_history_filename = join(figspath, "train_history.pkl")
     accuracies_filename = join(figspath, "svc_accuracies.pkl")
     if isfile(train_history_filename):
-        val_accuracies[model] = pd.read_pickle(train_history_filename)["val_accuracy"]
+        val_accuracies[model] = pd.read_pickle(train_history_filename)["val_accuracy"].rolling(3, min_periods=1).mean()
     if isfile(accuracies_filename):
         test_accuracies[model] = pd.read_pickle(accuracies_filename)["accuracy"]
 
-val_plot = val_accuracies.plot(title="Validation accuracy (Seen data)", xlabel="Steps", ylabel="Accuracy [%]").get_figure()
+val_plot = val_accuracies.plot(title="Validation accuracy (SVC on small validation dataset)", xlabel="Steps", ylabel="Accuracy").get_figure()
 val_plot.savefig("figs/val_acc_all.png")
 
-test_plot = test_accuracies.plot(x="Dataset size", title="Test accuracy (SVC on unseen data)", xlabel="Training data used [%]", ylabel="Accuracy [%]").get_figure()
+test_plot = test_accuracies.plot(x="Dataset size", title="Test accuracy (SVC on unseen data)", xlabel="Training data used [%]", ylabel="Accuracy").get_figure()
 test_plot.savefig("figs/test_acc_all.png")
